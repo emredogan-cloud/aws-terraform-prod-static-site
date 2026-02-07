dde41131cb24a121fcbd8bce9fef478ba60fed5f
emredogan-cloud <ed897854@gmail.com>
2026-02-07 17:23:43 +0300

    The folder structure for prod-static-site was created and the "S3 Bucket and DynamoDB table resources" created in the "backend-bootstrap" folder were used to move the .tfstate file to AWS thanks to the backend S3 structure located in backend.tf.

---
+++ backend-bootstrap/main.tf
@@ -0,0 +1,46 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "state-bucket" {
+  bucket = "prod-site${random_id.suffix.hex}"
+  force_destroy = false
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_access" {
+  bucket = aws_s3_bucket.state-bucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.state-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypted" {
+  bucket = aws_s3_bucket.state-bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "state_table" {
+  name = "prod-site-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ backend-bootstrap/outputs.tf
@@ -0,0 +1,10 @@
+output "state_bucket_name" {
+  description = "backend bootstrap S3 Bucket Name"
+  value = aws_s3_bucket.state-bucket.id
+}
+
+output "lock_table_name" {
+  description = "backend bootstrap DynamoDB table name"
+  value = aws_dynamodb_table.state_table.name
+}
+
