dde2a3d45f8979e3940a04596b9483216843d4f3
emredogan-cloud <ed897854@gmail.com>
2026-02-08 05:26:12 +0300

    updated modules/cloudfront_oac/variables.tf

--- modules/cloudfront_oac/variables.tf
+++ modules/cloudfront_oac/variables.tf
@@ -1,22 +1,22 @@
 variable "origin_domain_name" {
-  description = "S3 Bucket'ın Regional Domain Adı (örn: bucket-name.s3.us-east-1.amazonaws.com)"
+  description = "S3 Bucket Regional Domain Name"
   type        = string
 }
 
 variable "default_root_object" {
-  description = "Site açılınca varsayılan olarak yüklenecek dosya"
+  description = "Deafult UserInterface"
   type        = string
   default     = "index.html"
 }
 
 variable "price_class" {
-  description = "CloudFront Fiyat Sınıfı (PriceClass_100, PriceClass_200, PriceClass_All)"
+  description = "CloudFront PriceClass (PriceClass_100 , PriceClass_200, PriceClass_All)"
   type        = string
   default     = "PriceClass_100"
 }
 
 variable "tags" {
-  description = "Kaynaklara eklenecek etiketler"
+  description = "added to resources tickets"
   type        = map(string)
   default     = {}
 }
