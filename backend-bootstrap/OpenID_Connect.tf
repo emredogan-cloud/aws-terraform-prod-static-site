8436be2d0e79dae225a1f96ae258cf517017c1e2
emredogan-cloud <ed897854@gmail.com>
2026-02-08 18:19:09 +0300

    feat: Added OIDC and CI/CD pipeline

---
+++ backend-bootstrap/OpenID_Connect.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub sertifika imzası
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-deploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:emredogan-cloud/aws-terraform-prod-static-site:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
