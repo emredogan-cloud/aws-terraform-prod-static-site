45a16d05a2ee32af1c5b9ac6b680cc967d3187e2
emredogan-cloud <ed897854@gmail.com>
2026-02-08 22:53:46 +0300

    fix: Remove duplicate CORS headers from Python code

--- modules/visitor/lambda/counter.py
+++ modules/visitor/lambda/counter.py
@@ -19,11 +19,6 @@ def lambda_handler(event, context):
         
         return {
             'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*', 
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'GET, OPTIONS'
-            },
             'body': json.dumps({'count': new_count})
         }
         
